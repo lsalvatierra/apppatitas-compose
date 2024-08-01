053a1e435eb745a599ca697d293e8ae75b96d52e
Administrador2 <daplicacionesmoviles1@gmail.com>
2024-07-31 19:02:15 -0500

    Menu de opciones terminado.

---
+++ app/src/main/java/pe/edu/idat/apppatitas_compose/core/utils/MenuItem.kt
@@ -0,0 +1,8 @@
+package pe.edu.idat.apppatitas_compose.core.utils
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class MenuItem(
+    val icon: ImageVector,
+    val title: String
+)
--- app/src/main/java/pe/edu/idat/apppatitas_compose/home/view/HomeScreen.kt
+++ app/src/main/java/pe/edu/idat/apppatitas_compose/home/view/HomeScreen.kt
@@ -1,20 +1,177 @@
 package pe.edu.idat.apppatitas_compose.home.view
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Backup
+import androidx.compose.material.icons.filled.CloudUpload
+import androidx.compose.material.icons.filled.Folder
+import androidx.compose.material.icons.filled.History
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.OfflinePin
+import androidx.compose.material.icons.filled.People
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.rememberNavController
+import kotlinx.coroutines.launch
+import pe.edu.idat.apppatitas_compose.R
+import pe.edu.idat.apppatitas_compose.core.utils.MenuItem
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun homeScreen(){
-    Box(modifier = Modifier
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val coroutineScope = rememberCoroutineScope()
+    val navController = rememberNavController()
+
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            DrawerContent(items = opcionesMenu(), onItemClick = { item ->
+                coroutineScope.launch {
+                    drawerState.close()
+                }
+                /*when (item.title) {
+                    "My Files" -> navController.navigate(Screen.MyFiles.route)
+                    "Shared with me" -> navController.navigate(Screen.SharedWithMe.route)
+                    // Manejar otros elementos del menú si es necesario
+                }*/
+            })
+        },
+        content = {
+            Column {
+                TopAppBar(
+                    colors = TopAppBarDefaults.topAppBarColors(
+                        containerColor = Color.White,
+                        titleContentColor = Color.Black
+                    ),
+                    title = { Text("Patitas APP") },
+                    navigationIcon = {
+                        IconButton(onClick = {
+                            coroutineScope.launch {
+                                drawerState.open()
+                            }
+                        }) {
+                            Icon(Icons.Default.Menu, contentDescription = "Menu")
+                        }
+                    }
+                )
+                /*NavHost(
+                    navController = navController,
+                    startDestination = Screen.MyFiles.route,
+                    modifier = Modifier.padding(it)
+                ) {
+                    composable(Screen.MyFiles.route) { MyFilesScreen() }
+                    composable(Screen.SharedWithMe.route) { SharedWithMeScreen() }
+                    // Agrega otras rutas aquí si es necesario
+                }*/
+            }
+        }
+    )
+
+}
+
+@Composable
+fun DrawerContent(
+    items: List<MenuItem>,
+    onItemClick: (MenuItem) -> Unit
+) {
+    Column(Modifier
         .fillMaxSize()
-        .background(Color.Red)){
-        Text(text = "Bienvenido !!",
-            modifier = Modifier.align(Alignment.Center))
+        .background(Color.White)
+        .systemBarsPadding()) {
+        // Header
+        UserHeader()
+        Spacer(modifier = Modifier.height(8.dp))
+        // Menu items
+        items.forEach { item ->
+            DrawerMenuItem(item = item, onItemClick = onItemClick)
+        }
     }
+}
+
+@Composable
+fun UserHeader() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.imgperfil), // Reemplaza con tu imagen
+            contentDescription = null,
+            modifier = Modifier
+                .size(64.dp)
+                .clip(CircleShape)
+        )
+        Spacer(modifier = Modifier.width(16.dp))
+        Column {
+            Text(text = "Sandra Adams", fontWeight = FontWeight.Bold)
+            Text(text = "sandra_a88@gmail.com", color = Color.Gray)
+        }
+    }
+}
+
+@Composable
+fun DrawerMenuItem(
+    item: MenuItem,
+    onItemClick: (MenuItem) -> Unit
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable { onItemClick(item) }
+            .padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(imageVector = item.icon, contentDescription = null)
+        Spacer(modifier = Modifier.width(16.dp))
+        Text(text = item.title)
+    }
+}
+
+
+fun opcionesMenu(): List<MenuItem>{
+    val menuItems = listOf(
+        MenuItem(Icons.Default.Folder, "My Files"),
+        MenuItem(Icons.Default.People, "Shared with me"),
+        MenuItem(Icons.Default.Star, "Starred"),
+        MenuItem(Icons.Default.History, "Recent"),
+        MenuItem(Icons.Default.OfflinePin, "Offline"),
+        MenuItem(Icons.Default.CloudUpload, "Uploads"),
+        MenuItem(Icons.Default.Backup, "Backups")
+    )
+    return menuItems
 }
